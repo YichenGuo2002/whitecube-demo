8d854d3f63d52e2ca7c37d8ef022a83e5f4d443d
Yichen Guo <yg2258@nyu.edu>
2023-07-31 19:00:59 -0400

    Created sections

--- src/App.js
+++ src/App.js
@@ -4,20 +4,95 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <nav>
+        Artists
+        Exhibitions
+        Collect
+        Shop
+        WHITE CUBE
+      </nav>
+
+      <title>
+        Anselm Kiefer
+        Finnegans Wake
+        7 June – 20 August 2023
+        London
+        Visit exhibition
+      </title>
+
+      <div>
+        GALLERY EXHIBITION
+
+        Cinga Samson
+
+        Nzulu yemfihlakalo
+
+        7 July – 26 August 2023
+
+        Mason's Yard, London
+
+        Samson’s new series of dreamlike oil paintings speak to the fusion of material and metaphysical realms, giving form to the intangible and provoking an encounter with the vast unknown.
+        
+        Visit exhibition
+      </div>
+
+      <div>
+        GALLERY EXHIBITION
+
+        New Moroism
+
+        31 May – 9 September 2023
+
+        Hong Kong
+
+        Informed by their shared East Asian heritage, artists Michael Ho, Chris Huen Sin Kan, Timothy Lai and Su Yu-Xin each explore a new form of Moroism, an aesthetic paradigm characterised by ambiguity and indistinction.
+
+        Visit exhibition
+      </div>
+
+      <div>
+        ONLINE EXHIBITION
+
+        In the Shadow
+
+        19 July – 2 September 2023
+
+        Online
+
+        Drawing inspiration from da Vinci's notes, this exhibition looks at the phenomena of pareidolia – the tendency to perceive patterns and forms in unlikely places – as a valuable artistic device which enables imagination to rule over perception.
+      
+        View online
+      </div>
+      
+      <div>
+        PRESS RELEASE
+
+        Léon Wuidar joins White Cube
+
+        White Cube is pleased to announce representation of Belgian artist Léon Wuidar.
+
+        Find out more
+      </div>
+      
+      <div>
+        SALON
+
+        Anselm Kiefer
+        
+        We invite you to explore our current online presentation of secondary market work in our monthly Salon programme. To date, the gallery’s secondary market presentations have included notable artworks by Jean-Michel Basquiat, Alexander Calder, Carmen Herrera and Andy Warhol among others.
+        
+        Explore online
+      </div>
+      
+      <div>
+        NEWSLETTER
+
+        Be the first to know about our events, exhibitions, artists and much more.
+
+        Subscribe
+      </div>
+
     </div>
   );
 }
